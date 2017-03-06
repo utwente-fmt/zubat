194fbbd58091313f25030a3bad9c3e8d6220dbd3
djhuistra <djhuistra@gmail.com>
2017-03-06 13:59:42 +0100

    Assingment Part 1

---
+++ src/main/java/assignment/part_1/WGCSUL.java
@@ -0,0 +1,25 @@
+package assignment.part_1;
+
+import de.learnlib.api.SUL;
+import de.learnlib.api.SULException;
+
+public class WGCSUL implements SUL<String, String> {    
+    
+    
+	@Override
+	public void pre() {
+		// TODO: Implement model reset
+	}
+	
+	@Override
+	public void post() {
+	}
+
+	@Override
+	public String step(String input) throws SULException {
+		// TODO: Implement action, return output string.
+		return "";
+	}
+}
+
+
